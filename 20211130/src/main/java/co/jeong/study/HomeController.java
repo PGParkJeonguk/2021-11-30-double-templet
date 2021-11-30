0e13dddec8d542d3685a67dfa0445f928c22cb63
admin <admin@YD02-22>
2021-11-30 17:50:54 +0900

    2021-11-30 두개 템플릿사용

---
+++ 20211130/src/main/java/co/jeong/study/HomeController.java
@@ -0,0 +1,21 @@
+package co.jeong.study;
+
+import java.util.Locale;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HomeController {
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/home.do", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+
+		return "home/home";
+	}
+	
+}
